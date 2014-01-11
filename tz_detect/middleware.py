3a5bc2123fd2fb134b9f9c68e4e70cb8cebe53e9
Ivan Vershigora <limpbrains@mail.ru>
2014-01-12 00:02:44 +0400

    #7 store offset instead of timezone in user session

--- tz_detect/middleware.py
+++ tz_detect/middleware.py
@@ -1,5 +1,7 @@
 from django.utils import timezone
 
+from tz_detect.utilities import offset_to_timezone
+
 
 class TimezoneMiddleware(object):
     def process_request(self, request):
@@ -7,6 +9,7 @@ class TimezoneMiddleware(object):
         if tz:
             # `request.timezone_active` is used in the template
             # tag to detect if the timezone has been activated
+            tz = offset_to_timezone(tz)
             request.timezone_active = True
             timezone.activate(tz)
         else:
--- tz_detect/views.py
+++ tz_detect/views.py
@@ -1,8 +1,6 @@
 from django.http import HttpResponse
 from django.views.generic import View
 
-from tz_detect.utilities import offset_to_timezone
-
 
 class SetOffsetView(View):
     http_method_names = ['post']
@@ -10,14 +8,13 @@ class SetOffsetView(View):
     def post(self, request, *args, **kwargs):
         offset = request.POST.get('offset', None)
         if not offset:
-            return HttpResponse("No 'offset' parameter provided", status=400)
+            return HttpResponse("No 'offset' parameter provided", status_code=400)
 
         try:
             offset = int(offset)
         except ValueError:
-            return HttpResponse("Invalid 'offset' value provided", status=400)
+            return HttpResponse("Invalid 'offset' value provided", status_code=400)
 
-        tz = offset_to_timezone(int(offset))
-        request.session['detected_tz'] = tz
+        request.session['detected_tz'] = int(offset)
 
         return HttpResponse("OK")
