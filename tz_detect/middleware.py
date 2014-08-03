433181d03a1986304bf4eefad4e71280c581afc4
Ivan Vershigora <limpbrains@mail.ru>
2014-08-03 14:47:20 +0400

    get timezone from user profile

--- tz_detect/middleware.py
+++ tz_detect/middleware.py
@@ -5,6 +5,13 @@ from tz_detect.utilities import offset_to_timezone
 
 class TimezoneMiddleware(object):
     def process_request(self, request):
+        if request.user.is_authenticated():
+            profile = request.user.get_profile()
+            if profile.timezone:
+                timezone.activate(profile.timezone)
+                request.timezone_active = True
+                return
+
         tz = request.session.get('detected_tz')
         if tz:
             # `request.timezone_active` is used in the template
